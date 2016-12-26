9f98fd620dc9a4718a358f66e4be875038a7f19f
Katarina <Sweetkaterina1995@gmail.com>
2016-12-26 18:14:16 +0200

    native tree search

--- JavaScript/3-native.js
+++ JavaScript/3-native.js
@@ -14,3 +14,17 @@ let n2 = new Node(root, 'n2' );
 let n3 = new Node(root, 'n3' );
 
 console.dir(root, { depth: null });
+
+function find(tree, query, nodeName){
+	
+	for(let node in tree){
+		if( typeof tree[node] === 'object'){
+			
+			if(!nodeName)return tree[node];
+			else if(node==nodeName)return tree[node];
+			
+			var res = find(tree[node], query, nodeName);
+			if(res)return res;
+		}
+	}
+}
